b32baeaecebe71204867a88e76c1639b69e1fe70
azier001 <mutaahmad2711@gmail.com>
2023-10-21 23:55:47 +0700

    Day73-Responsive Login Form with Password Show/Hide Password using HTML CSS & JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+const showHide = document.getElementById('show-hide');
+const passwordInput = document.getElementById('password');
+
+showHide.addEventListener('click', function () {
+  showHide.classList.toggle('show');
+
+  if (showHide.classList.contains('show')) {
+    showHide.classList.remove('fa-eye-slash');
+    showHide.classList.add('fa-eye');
+    passwordInput.setAttribute('type', 'text');
+  } else {
+    showHide.classList.add('fa-eye-slash');
+    showHide.classList.remove('fa-eye');
+    passwordInput.setAttribute('type', 'password');
+  }
+});
